7ff001d4778320cdee36939d68b61531f8e66b64
Marija <maria5stojanovic@gmail.com>
2022-12-04 18:55:38 -0500

    adding nav bar and background color

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+//const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,7 +8,35 @@ module.exports = {
     "./app/views/**/*",
   ],
   theme: {
-    extend: {},
+    colors: {
+      orange: {
+        50: "#fff7ed",
+        100: "#ffedd5",
+        200: "#fed7aa",
+        300: "#fdba74",
+        400: "#fb923c",
+        500: "#f59e0b",
+        600: "#ea580c",
+        700: "#c2410c",
+      },
+      stone: {
+        50: "#fafaf9",
+        100: "#f5f5f4",
+        200: "#e7e5e4",
+        300: "#d6d3d1",
+        400: "#a8a29e",
+        500: "#737373",
+        600: "#57534e",
+        700: "#44403c",
+        800: "#292524",
+        900: "#1c1917",
+      },
+    },
+    plugins: [],
+    extend: {
+      backgroundImage: {
+        mars: "url('mars_picture.jpg')",
+      },
+    },
   },
-  plugins: [],
 };
