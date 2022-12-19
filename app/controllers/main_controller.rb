96a6d28f1881b2ffe3f1b73a1357301b4d8170f5
Marija <maria5stojanovic@gmail.com>
2022-12-18 20:29:00 -0500

    installing gems, encryting the NASA API key

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,18 @@
+require "http"
 class MainController < ApplicationController
   def home
   end
 
   def about
   end
+
+  def picture_of_the_day
+  #   response = HTTP.get("https://api.nasa.gov/planetary/apod?api_key=NASA_KEY")
+  #   if response.status == 200
+  #     render './../views/main/picture_of_the_day'
+  #   else
+  #     flash[:error] = "Something is wrong"
+  #   end
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'main#home'
   get  '/about', to: "main#about"
+  get '/picture_of_the_day', to: "main#picture_of_the_day"
 end
