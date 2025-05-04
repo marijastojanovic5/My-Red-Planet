baf5bebe6c1cca71fa57c17ff16e622bfff9d734
Marija <maria5stojanovic@gmail.com>
2025-05-04 18:59:40 -0400

    add comment to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # devise_for :admins,  controllers: { sesions: "sessions" }
   root to: 'main#index'
   get  '/about', to: "main#about"
-  get '/picture_of_the_day', to: "main#picture_of_the_day"
+  get '/picture_of_the_day', to: "main#picture_of_the_day" #make a comment here
   get '/mars_weather', to: "main#mars_weather"
   
 end
