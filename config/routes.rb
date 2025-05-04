3648d4b76c228101569782d8b028511ec79fb9e8
Marija <maria5stojanovic@gmail.com>
2025-05-04 12:43:23 -0400

    changecontroller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # devise_for :admins,  controllers: { sesions: "sessions" }
   root to: 'main#index'
   get  '/about', to: "main#about"
+  # made more comments here
   get '/picture_of_the_day', to: "main#picture_of_the_day"
   get '/mars_weather', to: "main#mars_weather"
   
