fd075e902890ef1a9d20e4aa5f288b3f0bb011ee
Marija <maria5stojanovic@gmail.com>
2025-05-21 10:40:20 -0400

    remove about route and method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_for :admins,  controllers: { sesions: "sessions" }
   root to: 'main#index'
-  get  '/about', to: "main#about"
   # made more comments here
   get '/picture_of_the_day', to: "main#picture_of_the_day"
   get '/mars_weather', to: "main#mars_weather"
